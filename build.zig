b628d1957fcc9a6edc2ad81240a116160d54715e
Jimmi Holst Christensen <jhc@dismail.de>
2023-02-11 18:56:50 +0100

    Update to latest version of zig

--- build.zig
+++ build.zig
@@ -1,33 +1,26 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    b.setPreferredReleaseMode(.ReleaseSafe);
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
     const strip = b.option(bool, "strip", "") orelse false;
 
-    const exe = b.addExecutable("jng2-decrypt", "src/main.zig");
+    const exe = b.addExecutable(.{
+        .name = "jng2-decrypt",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
     exe.addCSourceFile("lib/tiny-aes-c/aes.c", &.{});
     exe.defineCMacro("AES256", "1");
     exe.addIncludePath("lib/tiny-aes-c/");
 
-    exe.addCSourceFile("lib/zip/src/zip.c", &.{
-        "-fno-sanitize=undefined",
-    });
+    exe.addCSourceFile("lib/zip/src/zip.c", &.{"-fno-sanitize=undefined"});
     exe.addIncludePath("lib/zip/src/");
 
     exe.linkLibC();
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
     exe.strip = strip;
     exe.install();
-
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
 }
